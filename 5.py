c564534f33570e1cc2838c07b53cc7f507c1eb87
Polina Opletina <100723797+dunklenichts@users.noreply.github.com>
2022-04-04 15:43:16 +0300

    Add files via upload

--- 5.py
+++ 5.py
@@ -1,30 +1,47 @@
 '''
-    5. Реализовать формирование списка, используя функцию range() и возможности генератора.
-    В список должны войти чётные числа от 100 до 1000 (включая границы). Нужно получить
-    результат вычисления произведения всех элементов списка.
-'''
-from functools import reduce
-
-'''
-    Функция, отвечающая за умножение
-'''
-
-def mult_func (a, b):
-    return a * b
-
-my_list = [element for element in range(100, 1001) if element % 2 == 0]
-print(f'Input list: {my_list}')
-
-'''
-    Именованные переменные:
-    my_list -- список с четными числами от 100 до 1000
-    n -- результат умножения первых двух чисел списка
-    mult_output -- итоговое число
+    5. Реализовать класс Stationery (канцелярская принадлежность).
+    - определить в нём атрибут title (название) и метод draw (отрисовка).
+      Метод выводит сообщение «Запуск отрисовки»;
+    - создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер);
+    - в каждом классе реализовать переопределение метода draw.
+      Для каждого класса метод должен выводить уникальное сообщение;
+    - создать экземпляры классов и проверить, что выведет описанный метод для каждого
+      экземпляра.
 '''
 
-n = my_list[0]
-for element in range(1, len(my_list)):
-    mult_output = reduce(mult_func, [n, element])
-    n = mult_output
+class Stationery():
+    def __init__(self, title):
+        self.title = title
+    # общий метод для отрисовки
+    def draw(self):
+        print('Drawing started')
 
-print(f'List multiplication is {mult_output}')
+class Pen(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+    # переопределение метода
+    def draw(self):
+        return f"You're drawing with {self.title}"
+
+class Pencil(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+    # переопределение метода
+    def draw(self):
+        return f"You're drawing with {self.title}"
+
+class Handle(Stationery):
+    def __init__(self, title):
+        super().__init__(title)
+    # переопределение метода
+    def draw(self):
+        return f"You're drawing with {self.title}"
+
+# создание экземпляров класса
+a = Pen('pen')
+b = Pencil('pencil')
+c = Handle('handle')
+# проверка методов
+print(a.draw())
+print(b.draw())
+print(c.draw())
