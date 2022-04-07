237d75845f1bd0f12390033752cdc629e2ab6b28
Polina Opletina <100723797+dunklenichts@users.noreply.github.com>
2022-04-07 11:07:18 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,32 +1,8 @@
-'''
-    1. Реализовать класс Matrix (матрица). Обеспечить перегрузку
-    конструктора класса (метод __init__()), который должен принимать
-    данные (список списков) для формирования матрицы.
-
-    Следующий шаг — реализовать перегрузку метода __str__() для вывода
-    матрицы в привычном виде.
-
-    Далее реализовать перегрузку метода __add__() для реализации
-    операции сложения двух объектов класса Matrix (двух матриц).
-    Результатом сложения должна быть новая матрица.
-'''
-
-class Matrix():
-    def __init__(self, param_1):
-        self.param_1 = param_1
-
-    def __str__(self):
-        for i in self.param_1:
-            print(i)
-        return ''
-
-    def __add__(self, other):
-        for i in range(len(self.param_1)):
-            for j in range(len(other.param_1)):
-                self.param_1[i][j] = self.param_1[i][j] + other.param_1[i][j]
-        return Matrix.__str__(self)
-
-mc = Matrix([[1, 2, 3], [4, 5, 6]])
-mc_2 = Matrix([[2, 3, 4], [5, 6, 7]])
-print(mc)
-print(mc + mc_2)
+name = input('Enter your name: ')
+print(f'Hello, {name}')
+number = input('Enter a number: ')
+number = int(number)
+if number:
+    print(f'Your number is {number}')
+else:
+    print('Enter a number')
--- 3.py
+++ 3.py
@@ -1,52 +1,7 @@
-'''
-    3. Реализовать программу работы с органическими клетками, состоящими из ячеек.
-    Необходимо создать класс Клетка. В его конструкторе инициализировать параметр,
-    соответствующий количеству ячеек клетки (целое число). В классе должны быть
-    реализованы методы перегрузки арифметических операторов: сложение (__add__()),
-    вычитание (__sub__()), умножение (__mul__()), деление (__truediv__()). Данные
-    методы должны применяться только к клеткам и выполнять увеличение, уменьшение,
-    умножение и целочисленное (с округлением до целого) деление клеток,
-    соответственно.
-'''
-
-class  Cell():
-    def __init__(self, cell_amount):
-        self.cell_amount = cell_amount
-        # if cell_amount.isdigit == True:
-        #     print ('')
-        # else:
-        #     print('Error')
-
-    def __str__(self):
-        return self.cell_amount * '*'
-
-    def __add__(self, other):
-        return Cell(self.cell_amount + other.cell_amount)
-
-    def __sub__(self, other):
-        if (self.cell_amount - other.cell_amount) < 0:
-            print ('Error')
-        else:
-            return Cell(self.cell_amount - other.cell_amount)
-
-    def __mul__(self, other):
-        return Cell(self.cell_amount * other.cell_amount)
-
-    def __truediv__(self, other):
-        return Cell(self.cell_amount // other.cell_amount)
-
-    def make_order(self, other):
-        output_str = ''
-        for element in range(self.cell_amount // other):
-            output_str += '*' * other + "\\n"
-        return output_str
-
-cell = Cell(7)
-cell_2 = Cell(2)
-
-print(f'Sum: {cell + cell_2}')
-print(f'Sub: {cell - cell_2}')
-print(f'Mul: {cell * cell_2}')
-print(f"Div: {cell / cell_2}")
-
-print(cell.make_order(7))
+n = input('Enter n: ')
+if n.isdigit():
+    n = int(n)
+    sum = n + (n * 10 + n) + (n * 100 + n * 10 + n)
+    print(f"{n} + {(n * 10 + n)} + {(n * 100 + n * 10 + n)} = {sum}")
+else:
+    print('Enter an integer n')
